49304e9e1c53ca55e3ba7bc83c623ac08c769f7b
HiroRin753 <chikuzen.0306@gmail.com>
2021-03-03 22:02:25 +0900

    seeds作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   mount_uploader :img_name, ImgNameUploader
+
+  enum sex: {男:0, 女:1} #enumを使うとプログラムからは文字列でアクセスでき、データベースには整数値で保存される属性を作成できる。
 end
